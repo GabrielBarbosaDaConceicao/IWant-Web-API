281ff4471fc3c35e5e596d3399040215a49454c2
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-03-19 12:14:50 -0300

    Refatorado os atibutos e foi criado um construtor Entity instanciando o Guid

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -1,8 +1,6 @@
-﻿namespace IWantApp.Domain.Products
+﻿namespace IWantApp.Domain.Products;
+
+public class Category : Entity
 {
-    public class Category
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-    }
+    public string Name { get; set; }
 }
