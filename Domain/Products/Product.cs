592605e29555507e264c38fd376b388df357b9fd
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-03-19 11:53:11 -0300

    Adicionado atributos na class Category / class Product

---
+++ Domain/Products/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Category Category { get; set; }
+    public string Description { get; set; }
+    public bool HasStock { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditedBy { get; set; }
+    public DateTime EditedOn { get; set; }
+}
