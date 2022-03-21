d0757e3b3c2a1c3c31e905dda47f637761de6c66
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-03-21 11:48:58 -0300

    Gereando e aplicando migrations

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -3,7 +3,7 @@
 public class Product : Entity
 {
     public string Name { get; set; }
-    public int CategoryId { get; set; }
+    public Guid CategoryId { get; set; }
     public Category Category { get; set; }
     public string Description { get; set; }
     public bool HasStock { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using IWantApp.Endpoints.Categories;
+using IWantApp.Infra.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
