3a70b486db200d3040d5f8b2d88563719e012570
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-03-23 10:54:14 -0300

    Salvando usiario final

---
+++ Endpoints/Employees/EmployeePost.cs
@@ -0,0 +1,28 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IWantApp.Endpoints.Employees;
+
+public class EmployeePost
+{
+    public static string Template => "/employees";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+
+    [HttpPost]
+    public static IResult Action(EmployeeRequest employeeRequest, UserManager<IdentityUser> userManager)
+    {
+        var user = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
+        var result = userManager.CreateAsync(user, employeeRequest.Password).Result;
+
+        if (!result.Succeeded)
+        {
+            return Results.BadRequest(result.Errors.First());
+        }
+
+        return Results.Created($"/employees/{user.Id}", user.Id);
+    }
+}
