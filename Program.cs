5b985b7ee1f8b870e03b0e58aad1e213f10990e3
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-03-23 08:59:13 -0300

    Add e Instalado Identity

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 using IWantApp.Endpoints.Categories;
 using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
+builder.Services.AddSqlServer<ApplicationDbContext>(
+    builder.Configuration["ConnectionString:IWantDb"]);
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
