5eb2914e4ceaee091e1c20f8b34e3f619e924e9e
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-03-29 10:01:44 -0300

    Salvar e consultar produtos

---
+++ Endpoints/Products/ProductRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace IWantApp.Endpoints.Categories.Products;
+
+public record ProductRequest(string Name, Guid CategoryId, string Description, bool HasStock, bool Active);
--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseSerilog((context, configuration) => {
+    configuration
+        .WriteTo.Console()
+        .WriteTo.MSSqlServer(
+            context.Configuration["ConnectionString:IWantDb"],
+                sinkOptions: new MSSqlServerSinkOptions()
+                {
+                    AutoCreateSqlTable = true,
+                    TableName = "LogAPI"
+                });
+});
+
 builder.Services.AddSqlServer<ApplicationDbContext>(
     builder.Configuration["ConnectionString:IWantDb"]);
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
