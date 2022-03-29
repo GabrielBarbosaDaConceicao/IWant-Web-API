6fd96630197ac0625303e949100d8a4b1461e4af
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-03-29 10:19:11 -0300

    Validando categoria

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -30,7 +30,7 @@ public class Product : Entity
         var contract = new Contract<Product>()
             .IsNotNullOrEmpty(Name, "Name")
             .IsGreaterOrEqualsThan(Name, 3, "Name")
-            .IsNotNull(Category, "Category")
+            .IsNotNull(Category, "Category", "Category not found")
             .IsNotNullOrEmpty(Description, "Description")
             .IsGreaterOrEqualsThan(Description, 3, Description)
             .IsNotNullOrEmpty(CreatedBy, "CreatedBy")
